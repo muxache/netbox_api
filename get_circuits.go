7a9e83fbba0145ab4c51f8781d1e0907cd21b560
Mikhail Cherezov <mikhail.cherezov@wifire.ru>
2021-06-11 11:10:31 +0300

    changed get_circuits

--- get_circuits.go
+++ get_circuits.go
@@ -2,22 +2,24 @@ package netbox_api
 
 import (
 	"encoding/json"
-	"fmt"
 
 	"github.com/muxache/netbox_api/controller/get_netbox"
 	"github.com/muxache/netbox_api/data_model/netbox"
 )
 
 //GetALLCircuits allows get all l2circuits from api 'https://netbox.ti.ru/api/circuits/circuits/'
-func GetALLCircuits(token string) {
+func GetALLCircuits(url, token string) []netbox.Netbox_Circuits_Get {
 	var (
-		url        string = "https://netbox.ti.ru/api/circuits/circuits/"
-		nbCircuits netbox.Netbox_Circuits_Get
+		//url        string = "https://netbox.ti.ru/api/circuits/circuits/"
+
+		resArr []netbox.Netbox_Circuits_Get
 	)
 	res := get_netbox.GetFromNetBox(url, token)
 	for _, r := range res.Results {
+		var nbCircuits netbox.Netbox_Circuits_Get
 		rByte, _ := json.Marshal(r)
 		json.Unmarshal(rByte, &nbCircuits)
-		fmt.Println(nbCircuits.Cid)
+		resArr = append(resArr, nbCircuits)
 	}
+	return resArr
 }
