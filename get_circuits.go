e3a41c92111cbc43f7fb7d5e68568ccf503d48fe
Mikhail Cherezov <mikhail.cherezov@wifire.ru>
2021-06-11 11:20:49 +0300

    renamed get_circuits

--- get_circuits.go
+++ get_circuits.go
@@ -7,8 +7,8 @@ import (
 	"github.com/muxache/netbox_api/data_model/netbox"
 )
 
-//GetALLCircuits allows get all l2circuits from api 'https://netbox.ti.ru/api/circuits/circuits/'
-func GetALLCircuits(url, token string) []netbox.Netbox_Circuits_Get {
+//GetALLCircuits allows get all l2circuits from api 'https://netbox.ti.ru/api/circuits/circuits/'.
+func GetCircuits(url, token string) []netbox.Netbox_Circuits_Get {
 	var (
 		//url        string = "https://netbox.ti.ru/api/circuits/circuits/"
 
